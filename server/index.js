0846db164fe6b45a5c7e5311cd2e4787cb760a80
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2022-09-15 20:24:03 -0700

    dawg watch added

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(cors())
+
+const PORT = 5000
+
+
+app.listen(PORT, console.log(`RUNNING @ PORT ${PORT}`))
